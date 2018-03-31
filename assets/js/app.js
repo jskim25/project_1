3273f45272079cbca513130be5059980390e10c3
Keith Fuller <36208707+keith66fuller@users.noreply.github.com>
2018-03-31 13:20:00 -0500

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
+
 $(document).ready(function () {
+
+    $.ajax({
+        url:    "https://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz1gbt99772tn_adn99&state=il&city=chicago&childtype=neighborhood",    
+        method: 'GET',
+        dataType: 'xml',
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    })
+    .done(function(result) {
+        console.log(result);
+    })
+})
