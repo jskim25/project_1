328cce5758247cd73cbffb1c9f26b4c23951aa72
James Kim <jskim025@gmail.com>
2018-04-07 13:07:47 -0500

    updated repo for gina

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,6 @@ $(document).on('click', '#submitBtn', function() {
     // run the function that gets the events to load
     getEvents();
 
-    
 });
 
 
@@ -45,12 +44,28 @@ function getEvents() {
         newRow.find("td:last").remove();
         newRow.find("td:last").remove();
         newRow.find("td:last").remove();
-        newRow.find("td:last").remove();
         // create new button element, append to newRow button element
         $("#todo-table").append(newRow);
-       
-    })
-    
-}
 
+        // firebase
+        var config = {
+            apiKey: "AIzaSyD1M7vn5mlncIUT5WxX973pMcArUMZG2_0",
+            authDomain: "project-1-5c6e9.firebaseapp.com",
+            databaseURL: "https://project-1-5c6e9.firebaseio.com",
+            projectId: "project-1-5c6e9",
+            storageBucket: "project-1-5c6e9.appspot.com",
+            messagingSenderId: "75312830432"
+            };
+    
+        firebase.initializeApp(config);
+    
+        console.log(config);
+
+        newRow = $("#todo-table").val().trim();
+
+        database.ref().set({
+            newRow: newRow,
+        });
+    })
+}
 
