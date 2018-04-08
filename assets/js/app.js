633c33fd4646e160f77b491e7f6343349cfdc483
Keith Fuller <36208707+keith66fuller@users.noreply.github.com>
2018-04-08 11:42:29 -0500

    sunday

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,64 @@
-$(document).on('change', '#cityName', function() {
-    // get value of city name input from user
-    cityName = $('#cityName').val();
-
-    // Show the map for the city
-    displayMap(cityName);
-    
-    // run the function that gets the events to load
-    getEvents();
-
-    
-});
-
-
-function getEvents() {
-    var oArgs = {
-        app_key: "5rkz73R4DCcvJcbn",
-        where: cityName,
-        page_size: 10,
-        sort_order: "popularity",
+$(document).ready(function(){
+    var config = {
+        apiKey: "AIzaSyB2H-wik-3ypKe_8vIQG8kNIlBdDDPtRX4",
+        authDomain: "nucbc-project1-1522799622407.firebaseapp.com",
+        databaseURL: "https://nucbc-project1-1522799622407.firebaseio.com",
+        projectId: "nucbc-project1-1522799622407",
+        storageBucket: "nucbc-project1-1522799622407.appspot.com",
+        messagingSenderId: "350036869082"
     };
+    firebase.initializeApp(config);
 
-    EVDB.API.call("/events/search", oArgs, function (oData) {
-        console.log(oData);
+    var database = firebase.database();
+    var cityNames = firebase.database().ref('/').get({cityNames});
+    console.log(`cityNames: ${cityNames}`)
 
-        for (i=0; i<oData.events.event.length; i++) {
 
-            // all the elements needed for event table
-            var event = oData.events.event[i].title;
-            var startTime = oData.events.event[i].start_time;
-            var venue = oData.events.event[i].venue_name;
-            var address = oData.events.event[i].venue_address;
-            var city = oData.events.event[i].city_name;
-            var state = oData.events.event[i].region_abbr;
+    $(document).on('change', '#cityName', function() {
+        // get value of city name input from user
+        cityName = $('#cityName').val();
 
-            // var lat = oData.events.event[i].latitude;
-            // var lng = oData.events.event[i].longitude;
+        // Show the map for the city
+        displayMap(cityName);
+        
+        // run the function that gets the events to load
+        getEvents();
 
-            // append to table
-            $("#event-table > tbody").empty();
-            $("#event-table > tbody").append("<tr><td>" + event + "</td><td>" + startTime + "</td><td>" + venue + "</td><td>" + address + "</td><td>" + city + "</td><td>" + state + "</td></tr>");
+        
+    });
+
+
+    function getEvents() {
+        var oArgs = {
+            app_key: "5rkz73R4DCcvJcbn",
+            where: cityName,
+            page_size: 10,
+            sort_order: "popularity",
         };
-    })
-    
-}
 
+        EVDB.API.call("/events/search", oArgs, function (oData) {
+            console.log(oData);
+
+            for (i=0; i<oData.events.event.length; i++) {
+
+                // all the elements needed for event table
+                var event = oData.events.event[i].title;
+                var startTime = oData.events.event[i].start_time;
+                var venue = oData.events.event[i].venue_name;
+                var address = oData.events.event[i].venue_address;
+                var city = oData.events.event[i].city_name;
+                var state = oData.events.event[i].region_abbr;
+
+                // var lat = oData.events.event[i].latitude;
+                // var lng = oData.events.event[i].longitude;
+
+                // append to table
+                $("#event-table > tbody").empty();
+                $("#event-table > tbody").append("<tr><td>" + event + "</td><td>" + startTime + "</td><td>" + venue + "</td><td>" + address + "</td><td>" + city + "</td><td>" + state + "</td></tr>");
+            };
+        })
+        
+    }
+
+})
 
